77c7209d9113b1ca03b66a43b1dab5a80c6abf51
JoshVisentin <jvisentin1703@conestogac.on.ca>
2024-04-10 12:29:40 -0400

    changed print statements

--- RockPaperScissors/RockPaperScissors/RockPaperScissors.cpp
+++ RockPaperScissors/RockPaperScissors/RockPaperScissors.cpp
@@ -42,9 +42,9 @@ int main(void) {
         if (playerMove == computerMove) {
             printf("It's a tie!\n");
         } else if (computerMove + 1 == playerMove || computerMove - 2 == playerMove) {
-            printf("You win!\n");
+            printf("Nice! You win!\n");
         } else {
-            printf("You lose!\n");
+            printf("Ha Ha! You lose!\n");
         }
     }
 }
